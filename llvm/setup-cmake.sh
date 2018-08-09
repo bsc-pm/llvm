cf49f0e95dd45aa233c9cdec169bcd9bcb2f8dc0
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2018-08-09 09:57:04 +0200

    Improve setup-cmake.sh to autodetect the compiler and linker

--- llvm/setup-cmake.sh
+++ llvm/setup-cmake.sh
@@ -26,8 +26,24 @@ function info()
   nice_message "INFO" "$1"
 }
 
+function warning()
+{
+  nice_message "WARNING" "$1"
+}
+
+function run()
+{
+  P="$@"
+  echo "$P"
+  "$@"
+}
+
 BUILDDIR=$(readlink -f $(pwd))
 SRCDIR=$(dirname $(readlink -f $0))
+
+################################################################################
+# Detection of toolchain and sysroot
+################################################################################
 
 info "Using the current directory '${BUILDDIR}' as the build-dir"
 info "Using '${SRCDIR}' as the source-dir"
@@ -39,25 +55,34 @@ fi
 
 if [ ! -e "$GCC_TOOLCHAIN" ];
 then
-  die "GCC toolchain location '$GCC_TOOLCHAIN' does not exist"
+  die "GCC riscv64-unknown-linux-gnu toolchain location '$GCC_TOOLCHAIN' does not exist"
 fi
 
 if [ ! -d "$GCC_TOOLCHAIN" ];
 then
-  die "GCC toolchain location '$GCC_TOOLCHAIN' must be a directory"
+  die "GCC riscv64-unknown-linux-gnu toolchain location '$GCC_TOOLCHAIN' must be a directory"
 fi
 
-info "Using GCC toolchain at '$GCC_TOOLCHAIN'"
+info "Using GCC riscv64-unknown-linux-gnu toolchain at '$GCC_TOOLCHAIN'"
 
 RISCV_SYSROOT=${RISCV_SYSROOT:-"${GCC_TOOLCHAIN}/sysroot"}
 if [ ! -e "${RISCV_SYSROOT}" ];
 then
-  die "Linux sysroot at '${RISCV_SYSROOT}' not found. You can override it setting the RISCV_SYSROOT environment variable"
+  die "Linux RISC-V sysroot at '${RISCV_SYSROOT}' not found. You can override it setting the RISCV_SYSROOT environment variable"
 fi
+
+################################################################################
+# Detection of build system
+################################################################################
+
+# We only support Makefiles or Ninja. Ninja is better as it is able of finer
+# control
 
 BUILD_SYSTEM="Unix Makefiles"
 COMMAND_TO_BUILD="make -j$(nproc)"
 NINJA_BIN=${NINJA_BIN:-$(which ninja)}
+
+CMAKE_INVOCATION_EXTRA_FLAGS=""
 
 if [ "${NINJA_BIN}" = "" ];
 then
@@ -76,15 +101,115 @@ else
   fi
 fi
 
+################################################################################
+# Detection of compiler
+################################################################################
+
+# We only support clang or gcc. While the compilers are similar in speed, clang
+# allows using LLD which is noticeably faster than GNU ld
+
+if [ -z "${COMPILER}" ];
+then
+  info "Automatic detection of compiler. Override the detection setting the COMPILER enviromment variable to either 'gcc' or 'clang', in which case CC and CXX will be used by cmake instead"
+  if [ "$(which clang)" = "" ];
+  then
+    # Clang not found
+    if [ "$(which gcc)" != "" ];
+    then
+      COMPILER="gcc"
+      info "Using GCC $(gcc -dumpversion)"
+      info "gcc: $(which gcc)"
+      if [ "$(which g++)" != "" ];
+      then
+        info "g++: $(which g++)"
+        # Sanity check
+        if [ $(gcc -dumpversion) != $(g++ -dumpversion) ];
+        then
+          warning "gcc and g++ have different versions!"
+        fi
+        CC="$(which gcc)"
+        CXX="$(which g++)"
+        CMAKE_INVOCATION_EXTRA_FLAGS="-DCMAKE_C_COMPILER=$(which gcc) -DCMAKE_CXX_COMPILER=$(which g++)"
+      else
+        error "g++ not found in the PATH but gcc was found. This usually means that your system is missing development packages"
+      fi
+    fi
+  else
+    CLANG_VERSION=$(clang --version | head -n1 | sed 's/^.*version\s\+\([0-9]\+\(\.[0-9]\+\)\+\).*$/\1/')
+    COMPILER="clang"
+    info "Using clang ${CLANG_VERSION}"
+    info "clang: $(which clang)"
+    if [ "$(which clang++)" != "" ];
+    then
+      info "clang++: $(which clang++)"
+      # Sanity check
+      CLANGXX_VERSION=$(clang++ --version | head -n1 | sed 's/^.*version\s\+\([0-9]\+\(\.[0-9]\+\)\+\).*$/\1/')
+      if [ "$CLANG_VERSION" != "$CLANGXX_VERSION" ];
+      then
+        warning "clang and clang++ have different versions!"
+      fi
+      CC="$(which clang)"
+      CXX="$(which clang++)"
+      CMAKE_INVOCATION_EXTRA_FLAGS="-DCMAKE_C_COMPILER=$(which clang) -DCMAKE_CXX_COMPILER=$(which clang++)"
+    else
+      error "clang++ not found in the PATH but clang was found. You may have to review your installation"
+    fi
+  fi
+elif [ "${COMPILER}" = gcc ];
+then
+  CC=${CC:-"$(which gcc)"}
+  CXX=${CXX:-"$(which g++)"}
+elif [ "${COMPILER}" = clang ];
+then
+  CC=${CC:-"$(which clang)"}
+  CXX=${CXX:-"$(which clang++)"}
+fi
+
+################################################################################
+# Detection of the linker
+################################################################################
+
+# If using clang we try to use lld
+
+LINKER=gnu-ld
+if [ "$COMPILER" = "clang" ];
+then
+ if ( ${CC} -fuse-ld=lld -Wl,--version 2> /dev/null ) | grep -q "^LLD";
+ then
+   info "Using LLD"
+   LINKER=lld
+ else
+   info "Using GNU ld because we didn't find lld"
+ fi
+else
+  info "Using GNU ld because we are using gcc"
+fi
+
+if [ "$LINKER" = "lld" ];
+then
+  CMAKE_INVOCATION_EXTRA_FLAGS="${CMAKE_INVOCATION_EXTRA_FLAGS} -DLLVM_ENABLE_LLD=ON"
+fi
+
+if [ "$BUILD_SYSTEM" = "Ninja" ];
+then
+  info "Limiting concurrent linking jobs to 1"
+  CMAKE_INVOCATION_EXTRA_FLAGS="${CMAKE_INVOCATION_EXTRA_FLAGS} -DLLVM_PARALLEL_LINK_JOBS=1"
+else
+  warning "Makefiles do not allow limiting the concurrent linking jobs"
+fi
+
+################################################################################
+# cmake
+################################################################################
+
 info "Running cmake..."
-set -x
-cmake -G "${BUILD_SYSTEM}" ${SRCDIR} \
+run cmake -G "${BUILD_SYSTEM}" ${SRCDIR} \
    -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=RISCV \
    -DCMAKE_INSTALL_PREFIX=${INSTALLDIR} \
    -DLLVM_DEFAULT_TARGET_TRIPLE=riscv64-unknown-linux-gnu \
    -DDEFAULT_SYSROOT=${RISCV_SYSROOT} \
-   -DGCC_INSTALL_PREFIX=${GCC_TOOLCHAIN}
-set +x
+   -DGCC_INSTALL_PREFIX=${GCC_TOOLCHAIN} \
+   ${CMAKE_INVOCATION_EXTRA_FLAGS}
 
 if [ $? = 0 ];
 then
