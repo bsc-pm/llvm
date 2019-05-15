291c6ed3cbec38f0871363ad73080e0790722421
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2019-05-15 16:52:41 +0200

    Remove old EPI vector types
    Closes #16

--- clang/test/Sema/epi-vectors.c
+++ clang/test/Sema/epi-vectors.c
@@ -1,34 +1,6 @@
 // RUN: %clang_cc1 -triple riscv64 -target-feature +epi -ast-print %s \
 // RUN:    | FileCheck %s
 
-// Full register aliases
-void foo(void)
-{
-  // CHECK: __epi_4xf16 x1;
-  __epi_f16 x1;
-
-  // CHECK: __epi_2xf32 x2;
-  __epi_f32 x2;
-
-  // CHECK: __epi_1xf64 x3;
-  __epi_f64 x3;
-  //
-  // CHECK: __epi_8xi8 x4;
-  __epi_i8 x4;
-
-  // CHECK: __epi_4xi16 x5;
-  __epi_i16 x5;
-
-  // CHECK: __epi_2xi32 x6;
-  __epi_i32 x6;
-
-  // CHECK: __epi_1xi64 x7;
-  __epi_i64 x7;
-
-  // CHECK: __epi_1xi1 x8;
-  __epi_i1 x8;
-}
-
 // All lmul possibilities so far.
 void bar(void) {
   // CHECK: __epi_1xi64 x0;
