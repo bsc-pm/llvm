64ad1680102f47b98cc48ce78302a22773b31ec2
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2019-09-02 10:34:28 +0000

    Update test with atomics
    After a change in clang, these operations are not lowered in clang but
    later in LLVM.

--- clang/test/CodeGen/riscv-atomics.c
+++ clang/test/CodeGen/riscv-atomics.c
@@ -14,15 +14,12 @@
 #include <stdint.h>
 
 void test_i8_atomics(_Atomic(int8_t) * a, int8_t b) {
-  // RV32I:  call zeroext i8 @__atomic_load_1
-  // RV32I:  call void @__atomic_store_1
-  // RV32I:  call zeroext i8 @__atomic_fetch_add_1
-  // RV32IA: load atomic i8, i8* %a seq_cst, align 1
-  // RV32IA: store atomic i8 %b, i8* %a seq_cst, align 1
-  // RV32IA: atomicrmw add i8* %a, i8 %b seq_cst
-  // RV64I:  call zeroext i8 @__atomic_load_1
-  // RV64I:  call void @__atomic_store_1
-  // RV64I:  call zeroext i8 @__atomic_fetch_add_1
+  // RV32I: load atomic i8, i8* %a seq_cst, align 1
+  // RV32I: store atomic i8 %b, i8* %a seq_cst, align 1
+  // RV32I: atomicrmw add i8* %a, i8 %b seq_cst
+  // RV64I: load atomic i8, i8* %a seq_cst, align 1
+  // RV64I: store atomic i8 %b, i8* %a seq_cst, align 1
+  // RV64I: atomicrmw add i8* %a, i8 %b seq_cst
   // RV64IA: load atomic i8, i8* %a seq_cst, align 1
   // RV64IA: store atomic i8 %b, i8* %a seq_cst, align 1
   // RV64IA: atomicrmw add i8* %a, i8 %b seq_cst
@@ -32,15 +29,15 @@ void test_i8_atomics(_Atomic(int8_t) * a, int8_t b) {
 }
 
 void test_i32_atomics(_Atomic(int32_t) * a, int32_t b) {
-  // RV32I:  call i32 @__atomic_load_4
-  // RV32I:  call void @__atomic_store_4
-  // RV32I:  call i32 @__atomic_fetch_add_4
+  // RV32I: load atomic i32, i32* %a seq_cst, align 4
+  // RV32I: store atomic i32 %b, i32* %a seq_cst, align 4
+  // RV32I: atomicrmw add i32* %a, i32 %b seq_cst
   // RV32IA: load atomic i32, i32* %a seq_cst, align 4
   // RV32IA: store atomic i32 %b, i32* %a seq_cst, align 4
   // RV32IA: atomicrmw add i32* %a, i32 %b seq_cst
-  // RV64I:  call signext i32 @__atomic_load_4
-  // RV64I:  call void @__atomic_store_4
-  // RV64I:  call signext i32 @__atomic_fetch_add_4
+  // RV64I: load atomic i32, i32* %a seq_cst, align 4
+  // RV64I: store atomic i32 %b, i32* %a seq_cst, align 4
+  // RV64I: atomicrmw add i32* %a, i32 %b seq_cst
   // RV64IA: load atomic i32, i32* %a seq_cst, align 4
   // RV64IA: store atomic i32 %b, i32* %a seq_cst, align 4
   // RV64IA: atomicrmw add i32* %a, i32 %b seq_cst
@@ -56,9 +53,9 @@ void test_i64_atomics(_Atomic(int64_t) * a, int64_t b) {
   // RV32IA: call i64 @__atomic_load_8
   // RV32IA: call void @__atomic_store_8
   // RV32IA: call i64 @__atomic_fetch_add_8
-  // RV64I:  call i64 @__atomic_load_8
-  // RV64I:  call void @__atomic_store_8
-  // RV64I:  call i64 @__atomic_fetch_add_8
+  // RV64I: load atomic i64, i64* %a seq_cst, align 8
+  // RV64I: store atomic i64 %b, i64* %a seq_cst, align 8
+  // RV64I: atomicrmw add i64* %a, i64 %b seq_cst
   // RV64IA: load atomic i64, i64* %a seq_cst, align 8
   // RV64IA: store atomic i64 %b, i64* %a seq_cst, align 8
   // RV64IA: atomicrmw add i64* %a, i64 %b seq_cst
