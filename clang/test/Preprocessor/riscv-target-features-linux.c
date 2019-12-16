efc1c343660e7b8c59a1ed2483f36ddc5a3b8fca
Ferran Pallarès Roca <ferran.pallares@bsc.es>
2019-12-16 18:09:36 +0100

    Fix RV Linux preprocessor test

--- clang/test/Preprocessor/riscv-target-features-linux.c
+++ clang/test/Preprocessor/riscv-target-features-linux.c
@@ -1,11 +1,11 @@
 // Check that when no -march is used, riscv64-linux defines
-// the relevant RV64-MFDA features.
+// the relevant RV64-MFDAC (GC) features.
 // 
 // RUN: %clang -target riscv64-linux -x c -E -dM %s \
 // RUN: -o - | FileCheck %s
 
-// CHECK-NOT: __riscv_compressed
 // CHECK: __riscv_atomic 1
+// CHECK: __riscv_compressed 1
 // CHECK: __riscv_div 1
 // CHECK: __riscv_fdiv 1
 // CHECK: __riscv_flen 64
