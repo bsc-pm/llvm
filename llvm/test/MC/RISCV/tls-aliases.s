91b0ff8367be465612b1fc0bf641ffd14a8dd6d7
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2020-03-09 13:00:19 +0000

    Update tests
    Upstream changed the way llvm-objdump prints the tests

--- llvm/test/MC/RISCV/tls-aliases.s
+++ llvm/test/MC/RISCV/tls-aliases.s
@@ -5,7 +5,7 @@
 # RUN:   llvm-objdump -d -r - | \
 # RUN:   FileCheck --check-prefix=RV64 --check-prefix=CHECK %s
 
-# CHECK: .Lpcrel_hi0:
+# CHECK: <.Lpcrel_hi0>:
 # CHECK:   auipc a0, 0
 # CHECK: R_RISCV_TLS_GOT_HI20 y
 # RV32: lw  a0, 0(a0)
@@ -13,7 +13,7 @@
 # CHECK:  R_RISCV_PCREL_LO12_I .Lpcrel_hi0
 la.tls.ie	a0, y
 
-# CHECK: .Lpcrel_hi1:
+# CHECK: <.Lpcrel_hi1>:
 # CHECK:   auipc a0, 0
 # CHECK: R_RISCV_TLS_GOT_HI20 zero
 # RV32: lw  a0, 0(a0)
@@ -21,14 +21,14 @@ la.tls.ie	a0, y
 # CHECK:  R_RISCV_PCREL_LO12_I .Lpcrel_hi1
 la.tls.ie	a0, zero
 
-# CHECK: .Lpcrel_hi2:
+# CHECK: <.Lpcrel_hi2>:
 # CHECK: auipc	a0, 0
 # CHECK: R_RISCV_TLS_GD_HI20	y
 # CHECK: mv	a0, a0
 # CHECK: R_RISCV_PCREL_LO12_I	.Lpcrel_hi2
 la.tls.gd a0, y
 
-# CHECK: .Lpcrel_hi3:
+# CHECK: <.Lpcrel_hi3>:
 # CHECK: auipc	a0, 0
 # CHECK: R_RISCV_TLS_GD_HI20	zero
 # CHECK: mv	a0, a0
