d37e26f03a1f87aba1d6c6bfedc92cd8d1fdd711
Raúl Peñacoba Veigas <raul.penacoba@bsc.es>
2020-03-20 09:21:08 +0000

    Update IR test to include nanos6 reduction type constant enum

--- clang/test/OmpSs/IR/task_reduction.c
+++ clang/test/OmpSs/IR/task_reduction.c
@@ -6,10 +6,12 @@ void foo(int x) {
   {}
 }
 
-// CHECK: %0 = call token @llvm.directive.region.entry() [ "DIR.OSS"([5 x i8] c"TASK\00"), "QUAL.OSS.SHARED"(i32* %x.addr), "QUAL.OSS.DEP.REDUCTION"(i32* %x.addr, i32* %x.addr, i64 4, i64 0, i64 4), "QUAL.OSS.DEP.REDUCTION.INIT"(i32* %x.addr, void (i32*, i32*, i64)* @red_init), "QUAL.OSS.DEP.REDUCTION.COMBINE"(i32* %x.addr, void (i32*, i32*, i64)* @red_comb) ]
+// CHECK: %0 = call token @llvm.directive.region.entry() [ "DIR.OSS"([5 x i8] c"TASK\00"), "QUAL.OSS.SHARED"(i32* %x.addr), "QUAL.OSS.DEP.REDUCTION"(i32* %x.addr, i32 6000, i32* %x.addr, i64 4, i64 0, i64 4), "QUAL.OSS.DEP.REDUCTION.INIT"(i32* %x.addr, void (i32*, i32*, i64)* @red_init), "QUAL.OSS.DEP.REDUCTION.COMBINE"(i32* %x.addr, void (i32*, i32*, i64)* @red_comb) ]
 
 // CHECK: define internal void @red_init(i32* %0, i32* %1, i64 %2)
 // CHECK: store i32 0, i32* %3, align 4
 
 // CHECK: define internal void @red_comb(i32* %0, i32* %1, i64 %2)
-// CHECK: store i32 %add, i32* %3, align 4
+// CHECK: %add = add nsw i32 %7, %8
+// CHECK: store i32 %add, i32* %arrayctor.dst.cur, align 4
+
