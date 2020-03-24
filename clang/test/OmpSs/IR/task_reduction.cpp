d0fa4da2a7915d0dd153f2afaa77bb98e41abdac
Raúl Peñacoba Veigas <raul.penacoba@bsc.es>
2020-03-24 12:12:40 +0000

    Add a reduction reference test

---
+++ clang/test/OmpSs/IR/task_reduction.cpp
@@ -0,0 +1,17 @@
+// RUN: %clang_cc1 -verify -fompss-2 -disable-llvm-passes -ferror-limit 100 %s -S -emit-llvm -o - | FileCheck %s
+// expected-no-diagnostics
+
+void foo(int &rx) {
+    #pragma oss task reduction(+: rx)
+    {}
+}
+
+// CHECK: %1 = call token @llvm.directive.region.entry() [ "DIR.OSS"([5 x i8] c"TASK\00"), "QUAL.OSS.SHARED"(i32* %0), "QUAL.OSS.DEP.REDUCTION"(i32* %0, i32 6000, i32* %0, i64 4, i64 0, i64 4), "QUAL.OSS.DEP.REDUCTION.INIT"(i32* %0, void (i32*, i32*, i64)* @red_init), "QUAL.OSS.DEP.REDUCTION.COMBINE"(i32* %0, void (i32*, i32*, i64)* @red_comb) ]
+
+// CHECK: define internal void @red_init(i32* %0, i32* %1, i64 %2)
+// CHECK: store i32 0, i32* %3, align 4
+
+// CHECK: define internal void @red_comb(i32* %0, i32* %1, i64 %2)
+// CHECK: %add = add nsw i32 %7, %8, !dbg !16
+// CHECK: store i32 %add, i32* %arrayctor.dst.cur, align 4
+
