b65862148332cd93926c29a4478df23102cc0c24
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2020-04-15 10:46:19 +0000

    Fix wrong switch

--- clang/lib/Driver/ToolChains/Arch/RISCV.cpp
+++ clang/lib/Driver/ToolChains/Arch/RISCV.cpp
@@ -398,6 +398,7 @@ static bool getArchFeatures(const Driver &D, StringRef MArch,
       break;
     case 'b':
       Features.push_back("+experimental-b");
+      break;
     case 'v':
       Features.push_back("+v");
       break;
