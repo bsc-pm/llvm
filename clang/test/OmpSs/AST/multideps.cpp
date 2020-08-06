e8cbec918b2ffdfb303940cce651301c5daddfd6
Raúl Peñacoba Veigas <raul.penacoba@bsc.es>
2020-08-06 16:15:39 +0000

    Update AST/multideps.cpp test including AST of the template

--- clang/test/OmpSs/AST/multideps.cpp
+++ clang/test/OmpSs/AST/multideps.cpp
@@ -13,6 +13,21 @@ int main() {
     foo<int>();
 }
 
+// template
+// CHECK: OSSTaskDirective 0x{{.*}} <line:{{.*}}:{{.*}}, col:{{.*}}> ompss-2
+// CHECK-NEXT: OSSDependClause 0x{{.*}} <col:{{.*}}, col:{{.*}}> <oss syntax>
+// CHECK-NEXT: OSSMultiDepExpr 0x{{.*}} <col:{{.*}}, col:{{.*}}> 'int' lvalue
+// CHECK-NEXT: ArraySubscriptExpr 0x{{.*}} <col:{{.*}}, col:{{.*}}> 'int' lvalue
+// CHECK-NEXT: ImplicitCastExpr 0x{{.*}} <col:{{.*}}> 'int *' <ArrayToPointerDecay>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <col:{{.*}}> 'int [10]' lvalue Var 0x{{.*}} 'array' 'int [10]'
+// CHECK-NEXT: ImplicitCastExpr 0x{{.*}} <col:{{.*}}> 'int' <LValueToRValue>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <col:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <col:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
+// CHECK-NEXT: IntegerLiteral 0x{{.*}} <col:{{.*}}> 'int' 0
+// CHECK-NEXT: IntegerLiteral 0x{{.*}} <col:{{.*}}> 'int' 10
+// CHECK-NEXT: <<<NULL>>>
+
+// instantiation
 // CHECK: OSSTaskDirective 0x{{.*}} <line:{{.*}}:{{.*}}, col:{{.*}}> ompss-2
 // CHECK-NEXT: OSSDependClause 0x{{.*}} <col:{{.*}}, col:{{.*}}> <oss syntax>
 // CHECK-NEXT: OSSMultiDepExpr 0x{{.*}} <col:{{.*}}, col:{{.*}}> 'int' lvalue
@@ -29,4 +44,3 @@ int main() {
 // CHECK-NEXT: DeclRefExpr 0x{{.*}} <col:{{.*}}> 'int [10]' lvalue Var 0x{{.*}} 'array' 'int [10]'
 // CHECK-NEXT: OSSPrivateClause 0x{{.*}} <<invalid sloc>> <implicit>
 // CHECK-NEXT: DeclRefExpr 0x{{.*}} <col:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
-
