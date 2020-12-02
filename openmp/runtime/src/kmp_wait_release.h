62eda50c0ae4c801bf8e18fd3f54c653cde9fa2b
Roger Ferrer Ibanez <roger.ferrer@bsc.es>
2020-12-02 14:17:25 +0100

    Avoid setting task_team to NULL
    Otherwise the unshackled thread will give up too quickly as it will
    believe it can't get more tasks.

--- openmp/runtime/src/kmp_wait_release.h
+++ openmp/runtime/src/kmp_wait_release.h
@@ -379,7 +379,6 @@ final_spin=FALSE)
             unfinished_threads = &(task_team->tt.tt_unfinished_threads);
             /* kmp_int32 count = */KMP_ATOMIC_DEC(unfinished_threads);
             this_thr->th.th_task_team = NULL;
-            task_team = NULL;
             this_thr->th.th_reap_state = KMP_SAFE_TO_REAP;
         }
       } else {
