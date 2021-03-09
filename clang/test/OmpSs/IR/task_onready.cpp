343a722b67f0d58699bcdc76d6db1d74095e5457
Raúl Peñacoba Veigas <raul.penacoba@bsc.es>
2021-03-09 13:52:16 +0000

    Remove lbrace in onready IR test to make it pass

--- clang/test/OmpSs/IR/task_onready.cpp
+++ clang/test/OmpSs/IR/task_onready.cpp
@@ -50,7 +50,7 @@ void bar(int n) {
 
 // CHECK: %0 = call token @llvm.directive.region.entry() [ "DIR.OSS"([5 x i8] c"TASK\00"), "QUAL.OSS.SHARED"(%struct.S* %this1), "QUAL.OSS.ONREADY"(void (%struct.S*)* @compute_onready.8, %struct.S* %this1) ]
 
-// CHECK: define internal void @compute_onready(i32** %vla, i64 %0) {
+// CHECK: define internal void @compute_onready(i32** %vla, i64 %0)
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %vla.addr = alloca i32**, align 8
 // CHECK-NEXT:   %.addr = alloca i64, align 8
@@ -60,30 +60,30 @@ void bar(int n) {
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.1() {
+// CHECK: define internal void @compute_onready.1()
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %call = call i32 @_Z3fooIiET_v()
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.2(i32* %n) {
+// CHECK: define internal void @compute_onready.2(i32* %n)
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %n.addr = alloca i32*, align 8
 // CHECK-NEXT:   store i32* %n, i32** %n.addr, align 8
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.3() {
+// CHECK: define internal void @compute_onready.3()
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.4() {
+// CHECK: define internal void @compute_onready.4()
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.5(i32* %n) {
+// CHECK: define internal void @compute_onready.5(i32* %n)
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %n.addr = alloca i32*, align 8
 // CHECK-NEXT:   %agg.tmp.ensured = alloca %class.anon, align 8
@@ -93,7 +93,7 @@ void bar(int n) {
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.6(i32*** %vla) {
+// CHECK: define internal void @compute_onready.6(i32*** %vla)
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %vla.addr = alloca i32***, align 8
 // CHECK-NEXT:   store i32*** %vla, i32**** %vla.addr, align 8
@@ -102,13 +102,13 @@ void bar(int n) {
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.7() {
+// CHECK: define internal void @compute_onready.7()
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %call = call i32* @_Z3fooIPiET_v()
 // CHECK-NEXT:   ret void
 // CHECK-NEXT: }
 
-// CHECK: define internal void @compute_onready.8(%struct.S* %this) {
+// CHECK: define internal void @compute_onready.8(%struct.S* %this)
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   %this.addr = alloca %struct.S*, align 8
 // CHECK-NEXT:   store %struct.S* %this, %struct.S** %this.addr, align 8
