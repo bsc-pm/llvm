673faaeb06e8b9ce4a95a16a564321f6934a0f35
Raúl Peñacoba Veigas <raul.penacoba@bsc.es>
2022-03-16 16:01:00 +0000

    Only record lambda capture variables when building a wrapper function
    Fixes #149

--- clang/test/OmpSs/IR/lambda.cpp
+++ clang/test/OmpSs/IR/lambda.cpp
@@ -7,15 +7,44 @@ void bar(int x) {
       x++;
     };
     foo();
-    #pragma oss taskwait
+    #pragma oss task cost([&x]() { return x; }())
+    {}
 }
+// _ZZ3bari()
+// CHECK: %0 = getelementptr inbounds %class.anon, %class.anon* %foo, i32 0, i32 0
+// CHECK-NEXT: store i32* %x.addr, i32** %0, align 8
+// CHECK-NEXT: call void @"_ZZ3bariENK3$_0clEv"(%class.anon* noundef nonnull align 8 dereferenceable(8) %foo)
+// CHECK-NEXT: %1 = call token @llvm.directive.region.entry() [ "DIR.OSS"([5 x i8] c"TASK\00"), "QUAL.OSS.FIRSTPRIVATE"(i32* %x.addr), "QUAL.OSS.COST"(i32 (i32*)* @compute_cost, i32* %x.addr) ]
 
+// "_ZZ3bariENK3$_0clEv"()
 // CHECK: %1 = load i32*, i32** %0, align 8
 // CHECK-NEXT: %2 = call token @llvm.directive.region.entry() [ "DIR.OSS"([5 x i8] c"TASK\00"), "QUAL.OSS.SHARED"(i32* %1), "QUAL.OSS.DEP.IN"(i32* %1, [2 x i8] c"x\00", %struct._depend_unpack_t (i32*)* @compute_dep, i32* %1) ]
 // CHECK-NEXT: %3 = load i32, i32* %1, align 4
 // CHECK-NEXT: %inc = add nsw i32 %3, 1
 // CHECK-NEXT: store i32 %inc, i32* %1, align 4
 // CHECK-NEXT: call void @llvm.directive.region.exit(token %2)
+
+// CHECK: define internal i32 @compute_cost(i32* %x)
+// CHECK: entry:
+// CHECK-NEXT:   %x.addr = alloca i32*, align 8
+// CHECK-NEXT:   %ref.tmp = alloca %class.anon.0, align 8
+// CHECK-NEXT:   store i32* %x, i32** %x.addr, align 8
+// CHECK-NEXT:   %0 = getelementptr inbounds %class.anon.0, %class.anon.0* %ref.tmp, i32 0, i32 0
+// CHECK-NEXT:   store i32* %x, i32** %0, align 8
+// CHECK-NEXT:   %call = call noundef i32 @"_ZZ3bariENK3$_1clEv"(%class.anon.0* noundef nonnull align 8 dereferenceable(8) %ref.tmp)
+// CHECK-NEXT:   ret i32 %call
+// CHECK-NEXT: }
+
+// CHECK: define internal noundef i32 @"_ZZ3bariENK3$_1clEv"(%class.anon.0* noundef nonnull align 8 dereferenceable(8) %this)
+// CHECK: entry:
+// CHECK-NEXT:   %this.addr = alloca %class.anon.0*, align 8
+// CHECK-NEXT:   store %class.anon.0* %this, %class.anon.0** %this.addr, align 8
+// CHECK-NEXT:   %this1 = load %class.anon.0*, %class.anon.0** %this.addr, align 8
+// CHECK-NEXT:   %0 = getelementptr inbounds %class.anon.0, %class.anon.0* %this1, i32 0, i32 0
+// CHECK-NEXT:   %1 = load i32*, i32** %0, align 8
+// CHECK-NEXT:   %2 = load i32, i32* %1, align 4
+// CHECK-NEXT:   ret i32 %2
+// CHECK-NEXT: }
 
 // CHECK: define internal %struct._depend_unpack_t @compute_dep(i32* %x)
 // CHECK: entry:
@@ -31,5 +60,3 @@ void bar(int x) {
 // CHECK-NEXT:   %4 = load %struct._depend_unpack_t, %struct._depend_unpack_t* %retval, align 8
 // CHECK-NEXT:   ret %struct._depend_unpack_t %4
 // CHECK-NEXT: }
-
-
