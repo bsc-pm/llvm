fec96a94dcb2e51333c2dfb7c39189d2eba699cd
Raúl Peñacoba Veigas <rpenacob@bsc.es>
2023-05-18 12:40:29 +0000

    Fix previous commit. Introduce _OMPSS_NANOS6 only if we define _OMPSS_2

--- clang/test/OmpSs/Driver/driver.c
+++ clang/test/OmpSs/Driver/driver.c
@@ -4,8 +4,12 @@
 // RUN: %clang %s -c -E -dM -fompss-2=libnodes | FileCheck --check-prefix=USING-OMPSS-2-NODES %s
 // RUN: %clang %s -c -E -dM | FileCheck --check-prefix=NO-USING-OMPSS-2-NODES %s
 
-// RUN: %clang %s -c -E -dM -fompss-2=libnodes | FileCheck --check-prefix=USING-OMPSS-2-NANOS6 %s
+// RUN: %clang %s -c -E -dM -fompss-2=libnanos6 | FileCheck --check-prefix=USING-OMPSS-2-NANOS6 %s
 // RUN: %clang %s -c -E -dM | FileCheck --check-prefix=NO-USING-OMPSS-2-NANOS6 %s
+
+
+// USING-OMPSS-2: #define _OMPSS_2 1
+// NO-USING-OMPSS-2-NOT: #define _OMPSS_2
 
 // USING-OMPSS-2-NODES: #define _OMPSS_2
 // USING-OMPSS-2-NODES: #define _OMPSS_2_NODES 1
