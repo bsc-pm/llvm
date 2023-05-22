25e8cfbaf8449ff6ec0bad06231a86bdb56401f5
Raúl Peñacoba Veigas <rpenacob@bsc.es>
2023-05-22 16:36:10 +0000

    Introduce OMPSS2_RUNTIME env var to select the OmpSs-2 runtime. See more
    The priority comparing it with clang cmake option and -fompss-2= option
    is as follows
    
    (more priority) -fompss-2=  > OMPSS2_RUNTIME > CLANG_DEFAULT_OMPSS2_RUNTIME (less priority)
    
    Closes #184

--- clang/test/OmpSs/Driver/runtime.c
+++ clang/test/OmpSs/Driver/runtime.c
@@ -16,3 +16,25 @@
 // CHECK2: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
 // CHECK3: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
 // CHECK3: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
+
+// OMPSS2_RUNTIME priority check
+
+// CompileJob
+// RUN: OMPSS2_RUNTIME=libkaka %clang -fompss-2 %s -c -### 2>&1 | FileCheck %s --check-prefix=CHECK4
+// RUN: OMPSS2_RUNTIME=libkaka %clang -fompss-2=libasdf %s -c -### 2>&1 | FileCheck %s --check-prefix=CHECK5
+// LinkJob
+// RUN: touch %t.o
+// RUN: OMPSS2_RUNTIME=libkaka %clang -fompss-2 %t.o -### 2>&1 | FileCheck %s --check-prefix=CHECK6
+// RUN: OMPSS2_RUNTIME=libkaka %clang -fompss-2=libasdf %t.o -### 2>&1 | FileCheck %s --check-prefix=CHECK7
+// CompileAndLinkJob
+// RUN: OMPSS2_RUNTIME=libkaka %clang -fompss-2 %s -### 2>&1 | FileCheck %s --check-prefix=CHECK8
+// RUN: OMPSS2_RUNTIME=libkaka %clang -fompss-2=libasdf %s -### 2>&1 | FileCheck %s --check-prefix=CHECK9
+
+// CHECK4: clang: error: unsupported argument 'libkaka' to option '-fompss-2='
+// CHECK5: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
+// CHECK6: clang: error: unsupported argument 'libkaka' to option '-fompss-2='
+// CHECK7: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
+// CHECK8: clang: error: unsupported argument 'libkaka' to option '-fompss-2='
+// CHECK8: clang: error: unsupported argument 'libkaka' to option '-fompss-2='
+// CHECK9: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
+// CHECK9: clang: error: unsupported argument 'libasdf' to option '-fompss-2='
