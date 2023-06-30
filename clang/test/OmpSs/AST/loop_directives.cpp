93fb8813abd3d318584673bf18e54d2846e875db
Raúl Peñacoba Veigas <rpenacob@bsc.es>
2023-06-30 08:24:26 +0000

    Fix diagnostic of unroll clause. Add some AST and print tests for loop directives

---
+++ clang/test/OmpSs/AST/loop_directives.cpp
@@ -0,0 +1,41 @@
+// RUN: %clang_cc1 -verify -fompss-2 -ast-dump -ferror-limit 100 %s | FileCheck %s
+// expected-no-diagnostics
+template<typename T> T foo() { return T(); }
+
+void bar(int n) {
+  #pragma oss task
+  {
+    #pragma oss task for
+    for (int i = 0; i < 10; ++i) { }
+    #pragma oss taskloop
+    for (int i = 0; i < 10; ++i) { }
+    #pragma oss taskloop for
+    for (int i = 0; i < 10; ++i) { }
+    #pragma oss taskiter
+    for (int i = 0; i < 10; ++i) { }
+    int j = 0;
+    #pragma oss taskiter
+    while (j > 10) {}
+  }
+}
+
+// Data-sharing of induction variables
+
+// CHECK: OSSTaskDirective 0x{{.*}} <line:{{.*}}:{{.*}}, col:{{.*}}> ompss-2
+// CHECK-NEXT: CompoundStmt 0x{{.*}} <line:{{.*}}:{{.*}}, line:{{.*}}:{{.*}}>
+// CHECK: OSSTaskForDirective 0x{{.*}} <line:{{.*}}:{{.*}}, col:{{.*}}> ompss-2
+// CHECK-NEXT: OSSPrivateClause 0x{{.*}} <<invalid sloc>> <implicit>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <line:{{.*}}:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
+// CHECK: OSSTaskLoopDirective 0x{{.*}} <line:10:13, <invalid sloc>> ompss-2
+// CHECK-NEXT: OSSPrivateClause 0x{{.*}} <<invalid sloc>> <implicit>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <line:{{.*}}:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
+// CHECK: OSSTaskLoopForDirective 0x{{.*}} <line:12:13, <invalid sloc>> ompss-2
+// CHECK-NEXT: OSSPrivateClause 0x{{.*}} <<invalid sloc>> <implicit>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <line:{{.*}}:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
+// CHECK: OSSTaskIterDirective 0x{{.*}} <line:{{.*}}:{{.*}}, col:{{.*}}> ompss-2
+// CHECK-NEXT: OSSFirstprivateClause 0x{{.*}} <<invalid sloc>> <implicit>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <line:{{.*}}:{{.*}}> 'int' lvalue Var 0x{{.*}} 'i' 'int'
+// CHECK: OSSTaskIterDirective 0x{{.*}} <line:{{.*}}:{{.*}}, col:{{.*}}> ompss-2
+// CHECK-NEXT: OSSFirstprivateClause 0x{{.*}} <<invalid sloc>> <implicit>
+// CHECK-NEXT: DeclRefExpr 0x{{.*}} <line:{{.*}}:{{.*}}2> 'int' lvalue Var 0x{{.*}} 'j' 'int'
+
---
+++ clang/test/OmpSs/Print/loop_directives.cpp
@@ -0,0 +1,42 @@
+// RUN: %clang_cc1 -verify -fompss-2 -ast-print %s | FileCheck %s
+// expected-no-diagnostics
+template<typename T> T foo() { return T(); }
+
+void bar(int n) {
+  #pragma oss task
+  {
+    #pragma oss task for
+    for (int i = 0; i < 10; ++i) { }
+    #pragma oss taskloop
+    for (int i = 0; i < 10; ++i) { }
+    #pragma oss taskloop for
+    for (int i = 0; i < 10; ++i) { }
+    #pragma oss taskiter
+    for (int i = 0; i < 10; ++i) { }
+    int j = 0;
+    #pragma oss taskiter
+    while (j > 10) {}
+  }
+}
+
+// CHECK:     #pragma oss task
+// CHECK-NEXT:         {
+// CHECK-NEXT:             #pragma oss task for
+// CHECK-NEXT:                 for (int i = 0; i < 10; ++i) {
+// CHECK-NEXT:                 }
+// CHECK-NEXT:             #pragma oss taskloop
+// CHECK-NEXT:                 for (int i = 0; i < 10; ++i) {
+// CHECK-NEXT:                 }
+// CHECK-NEXT:             #pragma oss taskloop for
+// CHECK-NEXT:                 for (int i = 0; i < 10; ++i) {
+// CHECK-NEXT:                 }
+// CHECK-NEXT:             #pragma oss taskiter
+// CHECK-NEXT:                 for (int i = 0; i < 10; ++i) {
+// CHECK-NEXT:                 }
+// CHECK-NEXT:             int j = 0;
+// CHECK-NEXT:             #pragma oss taskiter
+// CHECK-NEXT:                 while (j > 10)
+// CHECK-NEXT:                     {
+// CHECK-NEXT:                     }
+// CHECK-NEXT:         }
+
